f437d4e9bca013e27b8803b870b3338c1dbe30b6
ajb0211 <ajb0211@gmail.com>
2020-12-01 11:09:23 -0500

    day1

---
+++ src/main/scala/y2020/1_ReportRepair.scala
@@ -0,0 +1,31 @@
+package ajb0211.Advent.y2020
+
+import scala.io.Source
+
+object ReportRepair extends App {
+  val report: List[Int] = Source.fromResource("y2020/1.txt").getLines.toList.map(_.toInt)
+
+  @annotation.tailrec
+  def findComplementProduct(target: Int = 2020, report: List[Int], acc: Set[Int] = Set[Int]()): Unit = {
+    if (acc contains (target - report.head)){
+      println(report.head * (target-report.head))
+    } else {
+      findComplementProduct(target, report.tail, acc + report.head)
+    }
+  }
+
+  // not sure why this isn't tail recursive
+  def findTripleProduct(target: Int = 2020, report: List[Int], acc: Set[Int] = Set[Int]()): Unit = {
+    acc.foreach{ (i: Int) =>
+      if (acc contains (target - report.head - i)){
+        println(i*report.head*(target-i-report.head))
+        return
+      }
+    }
+    findTripleProduct(target, report.tail, acc + report.head)
+  }
+
+  findComplementProduct(2020, report)
+  findTripleProduct(2020, report)
+
+}
