7b8c974ff43a010b456ec941bf53412d1e1ce04e
ajb0211 <ajb0211@gmail.com>
2020-12-03 16:51:57 -0500

    day3

---
+++ src/main/scala/y2020/3_TobogganTrajectory.scala
@@ -0,0 +1,75 @@
+package ajb0211.Advent.y2020
+
+object Main extends App {
+  val toboggan = TobogganTrajectory("y2020/3.txt")
+
+  println(s"Part 1: ${toboggan.sled(3,1).sum}")
+  println
+
+  val traversals: Array[(Int,Int)] = Array(
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+  )
+
+  val results: Array[Int] = traversals.map{ case (right: Int, down: Int) =>
+    val out = toboggan.sled(right,down).sum
+    println(s"(Right: $right, Down: $down): $out")
+    out
+  }
+
+  // Long conversion is important because there will be integer overflow
+  // Final answer is product of all answers for traversal methods in traversals
+  println(s"Final answer: ${results.foldRight(1.toLong){_ * _}}")
+}
+
+object TobogganTrajectory {
+  type Hill = Array[Array[Int]]
+
+  /**
+   * Constructor in object that takes
+   *
+   * @param path
+   * @return TobogganTrajectory with path information loaded as a Hill
+   */
+  def apply(path: String): TobogganTrajectory = {
+    new TobogganTrajectory(readFile(path))
+  }
+
+  /**
+   *  Reads a text file and maps values to integers consistent with problem definition
+   *     . = open space => 0
+   *     # = tree       => 1
+   *  We are counting the number of trees so it is convenient to store these as 1
+   *
+   * @param path file in resources folder containing data
+   * @return Hill, see type
+   */
+  def readFile(path: String): Hill = io.Source.fromResource(path).getLines.toArray.map{ _.map{
+      (c: Char) => if (c=='#') 1 else 0
+      }.toArray
+  }
+
+}
+
+class TobogganTrajectory(val hill: Array[Array[Int]]) {
+  val hillWidth: Int = hill(0).length
+
+  /**
+   * Case class makes it easier to pass data around
+   * Note that x and y are in math coordinates, reverse of array coordinates
+   * @param x Position along row
+   * @param y position along column
+   */
+  case class Position(val x: Int, val y: Int) {
+    def at: Int = hill(y)(x)
+    def update(dx: Int, dy: Int): Position = Position((x + dx) % hillWidth, y + dy)
+  }
+
+  def sled(dx: Int, dy: Int): Iterator[Int] = Iterator.unfold((0, Position(0, 0))) {
+    case (i: Int, pos: Position) if (pos.y < hill.length) => Some((pos.at, (pos.at, pos.update(dx, dy))))
+    case _ => None
+  }
+}
