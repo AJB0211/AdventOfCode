3278e4a72ab755ca9efbfc04a6fbe3292508ab20
ajb0211 <ajb0211@gmail.com>
2020-12-12 16:58:32 -0500

    day 11

--- src/main/scala/y2020/11_SeatingSystem.scala
+++ src/main/scala/y2020/11_SeatingSystem.scala
@@ -11,7 +11,7 @@ object SeatingSystem extends App {
   println("Part 1: Number occupied after relaxation")
   println(part1)
 
-  val ferry2 = new Ferry("y2020/11_test.txt")
+  val ferry2 = new Ferry("y2020/11.txt")
 
   ferry2.relax2
   val part2 = ferry2.numOccupied
@@ -30,23 +30,25 @@ class Ferry(var seating: Array[Array[Char]]){
     '#' -> 1
   )
 
-  trait Seat{
-    val row: Int
-    val col: Int
+  class Seat(val row: Int, val col: Int){
 
     def isValid: Boolean = (row >= 0) && (col >= 0) && (row < height) && (col < width)
     def notValid: Boolean = !isValid
 
     def value: Char = seating(row)(col)
 
-    def isFloor: Boolean = value == ','
+    def isFloor: Boolean = value == '.'
     def notFloor:Boolean = !isFloor
 
     // occupation number
     def occNum: Int = seatVal(value)
 
-    def apply(row: Int, col: Int): Seat
-    protected def getAdjacent: IndexedSeq[Seat]
+    protected def getAdjacent: IndexedSeq[Seat] =
+      for (i <- -1 to 1;
+           j <- -1 to 1;
+           seat = new Seat(row + i, col + j)
+           // Make sure this seat is not being returned
+           if !((i==0) && (j==0)) && seat.isValid) yield seat
 
     def numAdjacent: Int = getAdjacent.map(_.occNum).sum
 
@@ -62,24 +64,15 @@ class Ferry(var seating: Array[Array[Char]]){
 
   }
 
-  private case class CloseSeat(row: Int, col: Int) extends Seat {
-    override protected def getAdjacent: IndexedSeq[Seat] =
-    for (i <- -1 to 1;
-         j <- -1 to 1;
-         seat = CloseSeat(row + i, col + j)
-         // Make sure this seat is not being returned
-         if !((i==0) && (j==0)) && seat.isValid) yield seat
-  }
-
-  private case class FarSeat(override val row: Int, override val col: Int) extends Seat {
+  private class FarSeat(row: Int, col: Int) extends Seat(row, col) {
     protected def firstInDirection(di: Int, dj: Int): Option[FarSeat] = {
       var i = di
       var j = dj
 
       while (true){
-        val seat = FarSeat(row + i, row + j)
+        val seat = new FarSeat(row + i, col + j)
         if (seat.notValid) return None
-        else if (notFloor) return Some(seat)
+        else if (seat.notFloor) return Some(seat)
         else {i += di; j += dj}
       }
 
@@ -90,7 +83,7 @@ class Ferry(var seating: Array[Array[Char]]){
     override protected def getAdjacent: IndexedSeq[FarSeat] = {
       for (di <- -1 to 1;
            dj <- -1 to 1;
-           if (!((di == 0) && (dj == 0)))) yield firstInDirection(di, dj)
+           if !((di == 0) && (dj == 0))) yield firstInDirection(di, dj)
     }.flatten
 
     override def isSwap: Boolean =
@@ -106,13 +99,13 @@ class Ferry(var seating: Array[Array[Char]]){
     seating.map(_.mkString(" ")).mkString("\n")
   }
 
-  def iterate[T <: Seat](implicit seatClass: T.type): Boolean = {
+  def iterate: Boolean = {
     var swapFlag = false
     val tempSeating = seating.map(_.clone)
 
     for (i <- seating.indices;
          j <- seating(i).indices){
-      val seat = seatClass(i, j)
+      val seat = new Seat(i, j)
       if (seat.isSwap) {swapFlag = true; tempSeating(i)(j) = seat.swap}
     }
 
@@ -120,8 +113,22 @@ class Ferry(var seating: Array[Array[Char]]){
     swapFlag
   }
 
-  def relax: Unit = while(iterate(CloseSeat)){}
-  def relax2: Unit = while(iterate(FarSeat)){}
+  def iterate2: Boolean = {
+    var swapFlag = false
+    val tempSeating = seating.map(_.clone)
+
+    for (i <- seating.indices;
+         j <- seating(i).indices){
+      val seat = new FarSeat(i, j)
+      if (seat.isSwap) {swapFlag = true; tempSeating(i)(j) = seat.swap}
+    }
+
+    seating = tempSeating
+    swapFlag
+  }
+
+  def relax: Unit = while(iterate){}
+  def relax2: Unit = while(iterate2){}
 
   def numOccupied: Int = {
     for (i <- seating.indices;
